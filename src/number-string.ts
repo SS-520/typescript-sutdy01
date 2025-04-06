dc9bf0de7fc8289042f96c1ab0a5b445a6bd5426
rin <sayuri.shindo@gmail.com>
2025-04-06 20:38:35 +0900

    Number, String型の練習

---
+++ src/number-string.ts
@@ -0,0 +1,12 @@
+export {};
+
+let year: number = 1992;
+// year = false; // This will cause a type error
+console.log(year);
+
+let age: number = 32;
+console.log(age);
+
+let name: string = 'Ham';
+// name = 10; // This will cause a type error
+console.log(name);
