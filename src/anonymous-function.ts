e911fb0b0a5bfd7ac4e92fa646eb72cf1a7e4076
rin <sayuri.shindo@gmail.com>
2025-08-23 12:29:54 +0900

    TypeScriptの無名関数の定義の仕方を学習

---
+++ src/anonymous-function.ts
@@ -0,0 +1,15 @@
+export {};
+
+// 無名関数の定義・使い方
+let bmi: (height: number, weight: number) => number = function (
+  height: number,
+  weight: number
+): number {
+  return weight / (height * height);
+};
+/** 無名変数の格納先の変数のアノテーション
+ * 推論時→ let bmi: (height: number, weight: number) => number
+ * 推論ではなく厳密にアノテーションしたい場合は推論内容（：以降）を張り付けると○
+ */
+
+console.log(bmi(1.78, 86));
